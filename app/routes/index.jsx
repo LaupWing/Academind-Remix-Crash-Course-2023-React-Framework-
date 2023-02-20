8ebed3116c06589f6b2859d7a6f37cbaef482c24
LaupWing <loc_strict@hotmail.com>
2023-02-20 07:02:56 +0100

    Demo

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,5 +1,8 @@
 export default function Index() {
    return (
-      <h1>Welcome to Remix</h1>
+      <>
+         <h1>Welcome to Remix</h1>
+         <a href="/demo">Go to Demo page</a>
+      </>
    )
 }

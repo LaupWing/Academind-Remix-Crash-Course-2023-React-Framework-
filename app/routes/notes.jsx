e2bb13b1dde5cb45e0c57d5710d475d3b9ebba0f
LaupWing <loc_strict@hotmail.com>
2023-02-22 06:14:29 +0100

    action

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,4 +1,6 @@
+import { redirect } from "@remix-run/node"
 import NewNote, { links as newNoteLinks } from "~/components/NewNote"
+import { getStoredNotes, storeNotes } from "~/data/notes"
 
 const NotesPage = () => {
    return (
@@ -9,8 +11,14 @@ const NotesPage = () => {
 }
 export default NotesPage
 
-export function action() {
-   
+export async function action({ request }) {
+   const formData = await request.formData()
+   const noteDate = Object.entries(formData)
+   const existingNotes = await getStoredNotes()
+   noteDate.id = new Date().toISOString()
+   const updatedNotes = existingNotes.concat(noteDate)
+   await storeNotes(updatedNotes)
+   return redirect("/notes")
 }
 
 export function links() {

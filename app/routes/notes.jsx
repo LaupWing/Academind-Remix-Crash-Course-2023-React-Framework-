872fd1529c2e6f417d67f6f2c7ece51ebd4f1e1f
LaupWing <loc_strict@hotmail.com>
2023-02-22 06:30:17 +0100

    udpate

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,9 +1,11 @@
 import { redirect } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react"
 import NewNote, { links as newNoteLinks } from "~/components/NewNote"
 import NoteList, { links as noteListLinks } from "~/components/NoteList"
 import { getStoredNotes, storeNotes } from "~/data/notes"
 
 const NotesPage = () => {
+   const notes = useLoaderData()
    return (
       <main>
          <NewNote />
@@ -12,6 +14,12 @@ const NotesPage = () => {
    )
 }
 export default NotesPage
+
+export async function loader(){
+   const notes = await getStoredNotes()
+
+   return notes
+}
 
 export async function action({ request }) {
    const formData = await request.formData()
